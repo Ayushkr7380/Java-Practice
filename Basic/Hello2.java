2ee46c45d51ad628fe874d40d89886a87f6a274a
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-04 23:42:33 +0530

    hello2 file added

---
+++ Basic/Hello2.java
@@ -0,0 +1,16 @@
+class Ayush{
+    Ayush(){
+        System.out.println("I am Ayush.");
+    }
+}
+class Abhi{
+    Abhi(){
+        System.out.println("I am Abhi.");
+    }
+}
+public class Hello2 {
+   public static void main(String args[]){
+        Ayush ayush = new Ayush();
+        Abhi abhijeet = new Abhi();
+   } 
+}
