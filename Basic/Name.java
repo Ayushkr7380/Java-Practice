3594453dc51331e26909e4133a95ffb2624f1d7b
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-04 23:53:45 +0530

    Name.java file added

---
+++ Basic/Name.java
@@ -0,0 +1,30 @@
+class Ayu{
+    Ayu(String name){
+        System.out.println(name);
+    }
+}
+class Atul{
+    Atul(String name){
+        System.out.println(name);
+    }
+}
+class Abhi1{
+    Abhi1(String name){
+        System.out.println(name);
+    }
+}
+class Lucky{
+    Lucky(String name){
+        System.out.println(name);
+    }
+}
+public class Name {
+    public static void main(String args[]){
+        
+        //Create Objects
+        Ayu ayush = new Ayu("Ayush kumar");
+        Atul atul = new Atul("Atul sharma");
+        Abhi1 abhi = new Abhi1("Abhijeet raj");
+        Lucky lucky = new Lucky("Lucky raj");
+    }
+}
