b1a5eadf0c977fd367bc6939260123e228d047ff
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-05 13:18:38 +0530

    Add.java file added

---
+++ Basic/Add.java
@@ -0,0 +1,10 @@
+class Sum{
+    Sum(int n1,int n2){
+        System.out.println("Sum of "+n1+" and "+n2+" is "+(n1+n2));
+    }
+}
+public class Add {
+    public static void main(String args[]){
+        Sum sum = new Sum(12,3);
+    }
+}
