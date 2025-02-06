79996a6e8f390ac4d88a2372a07e9a439bce4aec
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-06 23:32:15 +0530

    Table.java file added

---
+++ Loops/Table.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Table1{
+    int num;
+    Table1(int n){
+        num = n;
+    }
+    void table(){
+        for(int i=1;i<=10;i++){
+            System.out.println(num+" X "+i+" = "+(num*i));
+        }
+    }
+}
+public class Table{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st number : ");
+        int num = sc.nextInt();
+        
+        Table1 t1 = new Table1(num);
+        t1.table();
+    }
+}
