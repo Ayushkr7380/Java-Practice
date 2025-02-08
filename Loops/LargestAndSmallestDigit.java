772753b41cac1e65560be1cedae63c14f9608a80
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-08 22:28:22 +0530

    LargestAndSmallestDigit.java file added

---
+++ Loops/LargestAndSmallestDigit.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class LargestAndSmallestDigit {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number : ");
+        int num = sc.nextInt();
+
+        int LargestDigit=0,Smallestdigit=9;
+        int temp = num;
+        while(temp!=0){
+            int check = temp % 10;
+            if(check > LargestDigit){
+                LargestDigit = check;
+            }
+            if(check < Smallestdigit){
+                Smallestdigit = check;
+            }
+            temp/=10;
+        }
+        System.out.println("Largest digit is "+LargestDigit);
+        System.out.println("Smallest digit is "+Smallestdigit);
+    }
+}
