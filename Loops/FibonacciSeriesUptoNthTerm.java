81df6358460b4a65acf847c5c80eaa31f9ef098f
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-09 23:10:07 +0530

    FibonacciSeriesUptoNthTerm.java file added

---
+++ Loops/FibonacciSeriesUptoNthTerm.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class FibonacciSeriesUptoNthTerm {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number : ");
+        int term = sc.nextInt();
+        int first = 0,second=1,next,i=0;
+        while(i<term){
+            next = first + second;
+            System.out.print(next+" ");
+            first = second;
+            second = next;
+
+            i++;
+        }
+    }
+}
