464aa189825b1a07ceff2049586d8287bc1fb681
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-10 23:59:18 +0530

    StringLength.java file updated

--- Strings/StringLength.java
+++ Strings/StringLength.java
@@ -2,12 +2,12 @@ import java.util.*;
 public class StringLength {
 public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
-    
+
     System.out.print("Enter any string : ");
     String str1 = sc.nextLine();
 
     int lengthOfString = str1.length();
 
-    System.out.println("Length of "+str1+" is "+lengthOfString);
+    System.out.println("Length of "+str1+" string is "+lengthOfString);
 }  
 }
