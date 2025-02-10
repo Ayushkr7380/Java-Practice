3a1b03d75eb45efc436c53ff48e1bdc3dc7b203d
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-10 23:49:37 +0530

    PrintName.java file added

---
+++ Strings/PrintName.java
@@ -0,0 +1,11 @@
+class PrintName{
+    public static void main(String args[]){
+        //Option 1..
+        String name = "Ayush kumar";
+        System.out.println("The name is "+name);
+
+        //Option 2..
+        String name2 = new String("Robert Downet jr.");
+        System.out.println("The hero is "+name2);
+    }
+}
