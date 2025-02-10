c14316236ed165d51bb6779b2ca8e7b58d6b96ad
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-10 23:53:08 +0530

    TakeStringFromUser.java file added

---
+++ Strings/TakeStringFromUser.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class TakeStringFromUser {
+ public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+    
+    System.out.print("Enter any string : ");
+    String str1 = sc.nextLine();
+
+    System.out.println("String is "+str1);
+ }   
+}
