73e611c523770430fa523d248da0645bf97bc049
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-11 22:51:58 +0530

    ReverseString.java file added

---
+++ Strings/ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ReverseString {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any string : ");
+        String str1 = sc.nextLine();
+
+        // Reverse the string using a loop
+        String reversed = "";
+        for (int i = str1.length() - 1; i >= 0; i--) {
+            reversed += str1.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + reversed);
+    }
+}
