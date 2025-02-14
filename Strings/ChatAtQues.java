35dd108ab85268a5337334dd9099fdc4063086e4
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-14 22:41:37 +0530

    ChatAtQues.java file added

---
+++ Strings/ChatAtQues.java
@@ -0,0 +1,6 @@
+public class ChatAtQues {
+    public static void main(String args[]){
+        String name="Ayush";
+        System.out.println("1st character of name is : "+name.charAt(0));
+    }    
+}
