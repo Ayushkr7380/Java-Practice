ea2db9ab07853301b83749ca2a79508989dbceaf
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-14 22:43:57 +0530

    ChatAtQues.java file updated

--- Strings/ChatAtQues.java
+++ Strings/ChatAtQues.java
@@ -1,6 +1,12 @@
 public class ChatAtQues {
     public static void main(String args[]){
         String name="Ayush";
-        System.out.println("1st character of name is : "+name.charAt(0));
+        // System.out.println("1st character of name is : "+name.charAt(0));
+
+
+        //Loop
+        for(int i=0;i<name.length();i++){
+            System.out.println("Character at postion "+(i+1)+" is "+name.charAt(i));
+        }
     }    
 }
