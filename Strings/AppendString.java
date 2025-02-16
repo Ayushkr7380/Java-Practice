7a263392d7146ab2f12e3898a3e5322abf2b15b6
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-17 00:01:45 +0530

    AppendString.java file added

---
+++ Strings/AppendString.java
@@ -0,0 +1,7 @@
+public class AppendString {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World"); // Appends a string
+        System.out.println(sb); // Output: Hello World
+    }
+}
