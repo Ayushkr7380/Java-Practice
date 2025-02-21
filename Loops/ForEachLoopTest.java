d41bb8235138b8c6df0e8d8dbbee58fd44665126
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-21 23:58:57 +0530

    ForEachLoopTest code added

---
+++ Loops/ForEachLoopTest.java
@@ -0,0 +1,10 @@
+class ForEachLoopTest{
+    public static void main(String args[]){
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Using for-each loop to iterate through the array
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
