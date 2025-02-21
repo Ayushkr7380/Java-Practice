07d664cedaec3b349fe97074ed04573d3e7635bd
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-22 00:22:52 +0530

    LengthOfStringsOfArray.java file added

---
+++ Array/LengthOfStringsOfArray.java
@@ -0,0 +1,8 @@
+public class LengthOfStringsOfArray {
+    public static void main(String args[]){
+        String names[] = {"Ayush","Abhijeet","Lucky","Atul"};
+        for (String name : names) {
+                System.out.println("Length of "+name+" is "+name.length());
+        }
+    }
+}
