507077e7696ffee62140c2b6ea77ae6a31ab9280
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-23 23:11:33 +0530

    PasswordValidation.java file updated

--- Basic/PasswordValidation.java
+++ Basic/PasswordValidation.java
@@ -1,15 +1,39 @@
-import java.util.*;
-public class PasswordValidation{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+public class PasswordValidation {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter password : ");
+        System.out.print("Enter password: ");
         String pwd = sc.nextLine();
         sc.close();
-        if(pwd.length() < 8){
-            System.out.println("length of password should be greater than 7.");
+
+        if (isValidPassword(pwd)) {
+            System.out.println("Password is valid.");
+        } else {
+            System.out.println("Password must contain at least 1 lowercase letter, 1 uppercase letter, 1 special character, 1 number, and be longer than 7 characters.");
         }
-        else{
-            System.out.println("Length of password is ok.");
+    }
+
+    public static boolean isValidPassword(String pwd) {
+        if (pwd.length() < 8) {
+            return false;
         }
+
+        boolean hasLower = false, hasUpper = false, hasNumber = false, hasSpecial = false;
+        String specialCharacters = "!@#$%^&*()-+=<>?/{}[]|\\:;\"'`~,._";
+
+        for (char ch : pwd.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                hasLower = true;
+            } else if (Character.isUpperCase(ch)) {
+                hasUpper = true;
+            } else if (Character.isDigit(ch)) {
+                hasNumber = true;
+            } else if (specialCharacters.contains(String.valueOf(ch))) {
+                hasSpecial = true;
+            }
+        }
+
+        return hasLower && hasUpper && hasNumber && hasSpecial;
     }
 }
