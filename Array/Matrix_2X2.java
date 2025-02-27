5297887800c8b9860947c9aa31339bccd915009a
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-27 22:59:08 +0530

    Matrix_2X2 code updated

--- Array/Matrix_2X2.java
+++ Array/Matrix_2X2.java
@@ -2,17 +2,34 @@ public class Matrix_2X2 {
     public static void main(String args[]){
         int mat1[][] = {{1,2},{54,6}};
         int mat2[][] = {{34,5},{1,78}};
-        int mat3[][] = {{0,0},{0,0}};
+        int matAdd[][] = {{0,0},{0,0}};
+        int matMul[][] = {{0,0},{0,0}};
 
+        //For matrix addition
         for(int i=0;i<2;i++){
             for(int j=0;j<2;j++){
-                mat3[i][j] = mat1[i][j] + mat2[i][j];
+                matAdd[i][j] = mat1[i][j] + mat2[i][j];
+            }
+        }
+        //For matrix multiplication
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                matMul[i][j] = mat1[i][j] * mat2[i][j];
             }
         }
 
+        System.out.println("Sum of two matrix is : ");
         for(int i=0;i<2;i++){
             for(int j=0;j<2;j++){
-                System.out.print(" "+mat3[i][j]);
+                System.out.print(" "+matAdd[i][j]);
+            }
+            System.out.println("");
+        }
+
+        System.out.println("Product of two matrix is : ");
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                System.out.print(" "+matMul[i][j]);
             }
             System.out.println("");
         }
