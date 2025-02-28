93f06c5dac95ee469ec68a95d5db1fa5946eabf1
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-28 22:50:29 +0530

    Matrix_2X2 code updated

--- Array/Matrix_2X2.java
+++ Array/Matrix_2X2.java
@@ -4,6 +4,7 @@ public class Matrix_2X2 {
         int mat2[][] = {{34,5},{1,78}};
         int matAdd[][] = {{0,0},{0,0}};
         int matMul[][] = {{0,0},{0,0}};
+        int matTranspose[][] = {{0,0},{0,0}};
 
         //For matrix addition
         for(int i=0;i<2;i++){
@@ -18,6 +19,15 @@ public class Matrix_2X2 {
             }
         }
 
+        //Transponse of matrix 1
+
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                matTranspose[j][i] = mat1[i][j];
+            }
+        }
+
+        //Display sum of two matrix
         System.out.println("Sum of two matrix is : ");
         for(int i=0;i<2;i++){
             for(int j=0;j<2;j++){
@@ -26,10 +36,20 @@ public class Matrix_2X2 {
             System.out.println("");
         }
 
+        //Display product of two matrix
         System.out.println("Product of two matrix is : ");
         for(int i=0;i<2;i++){
             for(int j=0;j<2;j++){
                 System.out.print(" "+matMul[i][j]);
+            }
+            System.out.println("");
+        }
+
+        //Display transpose of matrix 1
+        System.out.println("Transpose of matrix 1 is : ");
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                System.out.print(" "+matTranspose[i][j]);
             }
             System.out.println("");
         }
