48d15287d1f7e49375c21fab9e54f61a3fe6f7ef
Ayush Kumar <142900599+Ayushkr7380@users.noreply.github.com>
2025-03-20 19:01:59 +0530

    comment added

--- Strings/CapitalString.java
+++ Strings/CapitalString.java
@@ -2,10 +2,13 @@ import java.util.*;
 public class CapitalString {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+
+        //Prompt user to enter a string
         System.out.print("Enter any string : ");
         String str1 = sc.nextLine();
 
         //Capital the string 
+
         System.out.println("String in capital is "+str1.toUpperCase());
         
     }
