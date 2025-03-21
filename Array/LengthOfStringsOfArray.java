96502440a957c7bbf42d87666825cfa1a57b3eca
Ayush Kumar <142900599+Ayushkr7380@users.noreply.github.com>
2025-03-21 20:41:43 +0530

    convertStringtonumber.java file added

--- Array/LengthOfStringsOfArray.java
+++ Array/LengthOfStringsOfArray.java
@@ -1,5 +1,6 @@
 public class LengthOfStringsOfArray {
     public static void main(String args[]){
+
         String names[] = {"Ayush","Abhijeet","Lucky","Atul"};
         for (String name : names) {
                 System.out.println("Length of "+name+" is "+name.length());
