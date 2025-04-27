1dd370708e83e51b8ef081d7d69bcccf83189d1b
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-04-27 23:55:35 +0530

    comment added

--- College(MCA)/23-04-2025/Add.java
+++ College(MCA)/23-04-2025/Add.java
@@ -3,11 +3,15 @@ class Add{
     public static void main(String args[]){
         int a = 0,b =0;
         Scanner sc = new Scanner(System.in);
+        //Prompt user to Enter 1st number
         System.out.print("Enter 1st number : ");
         a = sc.nextInt();
+        //Prompt user to Enter 2nd number
         System.out.print("Enter 2nd number : ");
         b = sc.nextInt();
+        //Add a + b and store in c
         int c = a +b;
+        //Display c
         System.out.println("Sum is "+c);
     }
 }
