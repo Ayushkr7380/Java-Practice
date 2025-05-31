f9674a97b51a428054b34eece9ea81d09f27a396
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-05-31 21:11:25 +0530

    comment added

--- College(MCA)/InterfaceEx1.java
+++ College(MCA)/InterfaceEx1.java
@@ -1,21 +1,33 @@
-interface Human{
-    void longHair();
-    void smartness();
+// Define an interface named Human with two abstract methods
+interface Human {
+    void longHair();      // Method to describe hair characteristic
+    void smartness();     // Method to describe smartness
 }
-class Girl implements  Human{
+
+// Implement the Human interface in the Girl class
+class Girl implements Human {
+    
+    // Override the longHair method to provide implementation
     @Override
-    public void longHair(){
+    public void longHair() {
         System.out.println("This girl has long hair.");
     }
+
+    // Override the smartness method to provide implementation
     @Override
-    public void smartness(){
+    public void smartness() {
         System.out.println("This girl is very smart.");
     }
 }
-class InterfaceEx1{
-    public static void main(String args[]){
+
+// Main class to run the program
+class InterfaceEx1 {
+    public static void main(String args[]) {
+        // Create an object of Girl class
         Girl ayushi = new Girl();
-        ayushi.longHair();
-        ayushi.smartness(); 
+
+        // Call the implemented methods using the object
+        ayushi.longHair();     // Output: This girl has long hair.
+        ayushi.smartness();    // Output: This girl is very smart.
     }
 }
