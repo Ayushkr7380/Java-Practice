acf1c219bf2495865c50b2a1b5cb162ff0e1a061
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-06-04 23:47:59 +0530

    userdefined package in java

---
+++ College(MCA)/package/Main.java
@@ -0,0 +1,7 @@
+import ayushpackage.Greet;
+public class Main {
+    public static void main(String args[]){
+        Greet g1 = new Greet();
+        g1.hello();
+    }
+}
---
+++ College(MCA)/package/ayushpackage/Greet.java
@@ -0,0 +1,6 @@
+package ayushpackage;
+public class Greet{
+    public void hello(){
+        System.out.println("Hello from ayush package.");
+    }
+}
