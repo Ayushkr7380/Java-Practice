2d29d429d36792780358c6c8f418ca43fc15e872
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-06-07 22:43:07 +0530

    comment added

--- College(MCA)/package/Main.java
+++ College(MCA)/package/Main.java
@@ -1,21 +1,31 @@
+// Importing classes from the custom package 'ayushpackage'
 import ayushpackage.Greet;
 import ayushpackage.Operations;
+
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
+        // Creating an object of Greet class
         Greet g1 = new Greet();
+        // Calling the hello method to print a greeting
         g1.hello();
 
+        // Creating an object of Operations class to perform arithmetic operations
         Operations o1 = new Operations();
-        int sum = o1.addition(2,4);
-        System.out.println("Sum is "+sum);
 
+        // Performing addition of 2 and 4
+        int sum = o1.addition(2, 4);
+        System.out.println("Sum is " + sum);
+
+        // Performing subtraction of 4 from 2
         int sub = o1.subtraction(2, 4);
-        System.out.println("Subtraction is "+sub);
+        System.out.println("Subtraction is " + sub);
 
-        int mul = o1.multiplication(2,4);
-        System.out.println("Multiplication is "+mul);
+        // Performing multiplication of 2 and 4
+        int mul = o1.multiplication(2, 4);
+        System.out.println("Multiplication is " + mul);
 
-        int div = o1.division(2,4);
-        System.out.println("Division is "+div);
+        // Performing division of 2 by 4
+        int div = o1.division(2, 4);
+        System.out.println("Division is " + div);
     }
 }
